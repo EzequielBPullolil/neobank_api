042eb37e1ddf405cce9dc1e30a35a5128e105a6c
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 18:12:46 -0300

    Create pytest_configure function and modify singed_customer - Now singed_customer persist user wiouth use app.app_context - pytest_configure delete all records from the test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,36 @@
 import pytest
+from sqlalchemy import text
+from src.database import session
 from src.modules.customer.model import Customer
-from src.app import create_app, db
+from src.app import create_app
 import uuid
+
+
+def pytest_configure():
+    '''
+       Reset database deleting all rows from TEST database
+    '''
+    session.execute(
+        text('DELETE FROM "Customers"')
+    )
+    session.commit()
+    print('DATABASE RESET')
 
 
 @pytest.fixture()
 def app():
     app = create_app()
-
     yield app
 
 
 @pytest.fixture()
-def singed_customer(app):
+def singed_customer():
+    id = str(uuid.uuid4())
+    email = 'test@mail.com'
+    customer = Customer(id, email)
+    session.add(customer)
+    session.commit()
 
-    with app.app_context():
-        id = str(uuid.uuid4())
-        email = 'email@test.com'
-        customer = Customer(id=id, email=email)
-        db.session.add(customer)
-        db.session.commit()
     return {
         'id': id,
         'email': email
