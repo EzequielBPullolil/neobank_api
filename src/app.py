ca8d83de257805cafdf7127664d3320a075c767e
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 17:04:48 -0300

    We removed Flask-SQLAlchemy to better divide the app

--- src/app.py
+++ src/app.py
@@ -1,19 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
 import os
-db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-    db.init_app(app)
-
-    with app.app_context():
-        from src.modules.customer.model import Customer
-        db.create_all()
 
     return app
