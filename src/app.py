4ee1ccc565d0fe3e59e832a541b037fe9a6f1942
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 18:04:22 -0300

    Remove unused app env configs

--- src/app.py
+++ src/app.py
@@ -1,10 +1,6 @@
 from flask import Flask
-import os
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
     return app
