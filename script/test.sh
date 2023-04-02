c728da3b4d824d3e1547e4e2176f1e5f3bcbd50e
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 17:08:47 -0300

    Replace env varaible name of database uri

--- script/test.sh
+++ script/test.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH='.'
 export PYTHONENV='test'
 
-export SQLALCHEMY_DATABASE_URI='postgresql://drhades:SoyEzequielEnSql@localhost/neobank_test'
+export DATABASE_URI='postgresql://drhades:SoyEzequielEnSql@localhost/neobank_test'
 pytest -s -vvvv $1 $2
