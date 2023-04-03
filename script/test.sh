748a242fba8c2111e5b4f5a0fcb22f54dad64515
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 21:18:06 -0300

    Add test secret_key for hashing

--- script/test.sh
+++ script/test.sh
@@ -1,5 +1,6 @@
 export PYTHONPATH='.'
 export PYTHONENV='test'
 
+export SECRET_KEY='VkGE7lKTaemghlDP'
 export DATABASE_URI='postgresql://drhades:SoyEzequielEnSql@localhost/neobank_test'
 pytest -s -vvvv $1 $2
