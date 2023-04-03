bf503f97863d63ca0b8472e884005c47f729fde8
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 21:58:58 -0300

    Add column Alias to Customer model

--- src/modules/customer/model.py
+++ src/modules/customer/model.py
@@ -7,15 +7,17 @@ class Customer(Base):
     id = Column(String, primary_key=True)
     email = Column(String)
     dni = Column(String)
+    alias = Column(String)
 
-    def __init__(self, id, email, dni) -> None:
+    def __init__(self, id, email, dni, alias) -> None:
         super().__init__()
         self.id = id
         self.email = email
         self.dni = dni
+        self.alias = alias
 
     def __repr__(self) -> str:
-        return f"Customer(id={self.id!r}, email={self.email!r}, dni={self.dni!r}))"
+        return f"Customer(id={self.id!r}, email={self.email!r}, dni={self.dni!r},  alias={self.alias!r}))"
 
 
 Base.metadata.create_all(engine)
