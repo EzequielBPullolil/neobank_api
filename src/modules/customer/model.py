a4b047556133f66229f65f8086e91903ab9b6c31
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 21:06:56 -0300

    Added dni column to Customer model

--- src/modules/customer/model.py
+++ src/modules/customer/model.py
@@ -6,14 +6,16 @@ class Customer(Base):
     __tablename__ = 'Customers'
     id = Column(String, primary_key=True)
     email = Column(String)
+    dni = Column(String)
 
-    def __init__(self, id, email) -> None:
+    def __init__(self, id, email, dni) -> None:
         super().__init__()
         self.id = id
         self.email = email
+        self.dni = dni
 
     def __repr__(self) -> str:
-        return f"Customer(id={self.id!r}, email={self.email!r}))"
+        return f"Customer(id={self.id!r}, email={self.email!r}, dni={self.dni!r}))"
 
 
 Base.metadata.create_all(engine)
