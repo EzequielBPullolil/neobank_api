ff957ca39de56fa66e40dfc3e7c376e6f0824d98
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 21:03:18 -0300

    Add testcase for check if send already singed dni raise exception

--- tests/modules/customer/services/sing_up_test.py
+++ tests/modules/customer/services/sing_up_test.py
@@ -1,4 +1,4 @@
-from src.modules.customer.exceptions import DuplicatedEmail
+from src.modules.customer.exceptions import DuplicatedEmail, DuplicatedDNI
 from src.modules.customer.services.sing_up import SingUpCustomerService
 import pytest
 
@@ -10,7 +10,8 @@ class TestSingUpCustomerServices:
         '''
             Sings an user
         '''
-        self.sing_up_customer.register_user(email='abcdf@mail.com')
+        self.sing_up_customer.register_user(
+            email='abcdf@mail.com', dni='44160102')
 
     def test_already_singed_email_raise_exception(self, singed_customer):
         '''
@@ -18,8 +19,9 @@ class TestSingUpCustomerServices:
         '''
         registered_email = singed_customer['email']
         with pytest.raises(DuplicatedEmail) as e_info:
-            self.sing_up_customer.register_user(registered_email)
-            assert f"The email '{singed_customer['email']}' is already in use" in str(
+            self.sing_up_customer.register_user(
+                registered_email, dni='11234234')
+            assert f"The email '{registered_email}' is already in use" in str(
                 e_info.value)
 
     def test_already_singed_dni_raise_exception(self, singed_customer):
@@ -27,8 +29,10 @@ class TestSingUpCustomerServices:
             Check if parse an registered dni raise exception
         '''
         registered_dni = singed_customer['dni']
-        with pytest.raises(DuplicatedEmail) as e_info:
+        with pytest.raises(DuplicatedDNI) as e_info:
             self.sing_up_customer.register_user(
-                email='dniraise@email.com', dni=registered_dni)
-            assert f"The dni '{singed_customer['email']}' is already in use" in str(
+                email='dniraise@email.com',
+                dni=registered_dni
+            )
+            assert f"The dni '{registered_dni}' is already in use" in str(
                 e_info.value)
