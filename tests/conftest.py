b437fe3f9ff5b8c5c3bc74b002479516c91beb49
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 21:59:37 -0300

    add alias to signed_customer fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from src.database import session
 from src.modules.customer.model import Customer
 from src.app import create_app
 import uuid
-from modules.customer.helpers.hash_dni import hash_dni
+from src.modules.customer.helpers.hash_dni import hash_dni
 
 
 def pytest_configure():
@@ -29,8 +29,9 @@ def singed_customer():
     id = str(uuid.uuid4())
     email = 'test@mail.com'
     dni = '44170104'
+    alias = 'neobank_ezequiel'
     encripted_dni = hash_dni(dni)
-    customer = Customer(id, email, encripted_dni)
+    customer = Customer(id, email, encripted_dni, alias)
     session.add(customer)
     session.commit()
 
