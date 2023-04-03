8d82d6241a2387109051f9c1d9a32f7051584008
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 22:00:24 -0300

    Added functionality to the register user method - Now register_user generates an alias, hashes the DNI and persists the Customer to db

--- src/modules/customer/services/sing_up.py
+++ src/modules/customer/services/sing_up.py
@@ -1,4 +1,9 @@
 from src.modules.customer.db_utils import EmailManager, DniManager
+from src.modules.customer.helpers.generate_alias import generate_alias
+from src.database import session
+from src.modules.customer.model import Customer
+from uuid import uuid4
+from src.modules.customer.helpers.hash_dni import hash_dni
 
 
 class SingUpCustomerService():
@@ -12,3 +17,15 @@ class SingUpCustomerService():
         '''
         self.emailManager.check_email_availability(email)
         self.dniManager.check_dni_availability(dni)
+        hashed_dni =hash_dni(dni)
+        alias = generate_alias('ezequiel')
+        id = uuid4()
+        customer = Customer(
+            id,
+            email,
+            hashed_dni,
+            alias
+        )
+        session.add(customer)
+
+        session.commit()
