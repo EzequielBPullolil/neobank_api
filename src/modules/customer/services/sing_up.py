71ad4fa6e0da4f9e09ae84eac6338b023e02cb2b
EzequielK-source <programacionezequielk@gmail.com>
2023-04-02 21:05:33 -0300

    Add check_dni_availability method call in register_user method - Add dniManager property to SingUpCustomerService class - Add dni argument to register_user method

--- src/modules/customer/services/sing_up.py
+++ src/modules/customer/services/sing_up.py
@@ -1,12 +1,14 @@
-from src.modules.customer.helpers import EmailManager
+from src.modules.customer.db_utils import EmailManager, DniManager
 
 
 class SingUpCustomerService():
     emailManager = EmailManager()
+    dniManager = DniManager()
 
-    def register_user(self, email):
+    def register_user(self, email, dni):
         '''
             Check if email are avaible and persist 
             customer
         '''
         self.emailManager.check_email_availability(email)
+        self.dniManager.check_dni_availability(dni)
