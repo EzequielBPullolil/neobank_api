521d026f8e8a30e806c6537a326ea7232f8242b6
EzequielK-source <programacionezequielk@gmail.com>
2023-04-15 20:03:17 -0300

    Fixed a conftest singed_customer fixture issue - The singed_customer prop creates a persistence conflict because each call of singed_customer persists a user with the same email and dni

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,29 @@ from src.modules.customer.model import Customer
 from src.app import create_app
 import uuid
 from src.modules.customer.helpers.hash_dni import hash_dni
+from src.modules.customer.helpers.generate_cvu import generate_cvu
+
+email = 'test@mail.com'
+id = str(uuid.uuid4())
+dni = '44170104'
 
 
 def pytest_configure():
     '''
+
        Reset database deleting all rows from TEST database
     '''
+    global id, email, dni
     session.execute(
         text('DELETE FROM "Customers"')
     )
-    session.commit()
     print('DATABASE RESET')
+    alias = 'neobank_ezequiel'
+    cvu = generate_cvu()
+    encripted_dni = hash_dni(dni)
+    customer = Customer(id, email, encripted_dni, alias, cvu)
+    session.add(customer)
+    session.commit()
 
 
 @pytest.fixture()
@@ -26,15 +38,8 @@ def app():
 
 @pytest.fixture()
 def singed_customer():
-    id = str(uuid.uuid4())
-    email = 'test@mail.com'
-    dni = '44170104'
-    alias = 'neobank_ezequiel'
-    encripted_dni = hash_dni(dni)
-    customer = Customer(id, email, encripted_dni, alias)
-    session.add(customer)
-    session.commit()
 
+    global id, email, dni
     return {
         'id': id,
         'email': email,
