d29be4da933f8d6e0d6dce8c1ac033fe425457af
EzequielK-source <programacionezequielk@gmail.com>
2023-04-15 20:05:32 -0300

    Add cvu_identifier to Customer model and add arguments to the Columns - Add no nullable argument to email, dni and cvu_identifier - Add unique argument to email, dni and cvu_identifier

--- src/modules/customer/model.py
+++ src/modules/customer/model.py
@@ -5,16 +5,18 @@ from sqlalchemy import String, Column
 class Customer(Base):
     __tablename__ = 'Customers'
     id = Column(String, primary_key=True)
-    email = Column(String)
-    dni = Column(String)
+    email = Column(String, nullable=False, unique=True)
+    dni = Column(String, nullable=False, unique=True)
+    cvu_identifier = Column(String(22), nullable=False, unique=True)
     alias = Column(String)
 
-    def __init__(self, id, email, dni, alias) -> None:
+    def __init__(self, id, email, dni, alias, cvu) -> None:
         super().__init__()
         self.id = id
         self.email = email
         self.dni = dni
         self.alias = alias
+        self.cvu_identifier = cvu
 
     def __repr__(self) -> str:
         return f"Customer(id={self.id!r}, email={self.email!r}, dni={self.dni!r},  alias={self.alias!r}))"
